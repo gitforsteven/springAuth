af173d98e90e1159030f7b3ed0f5c6d010ec56fc
Park Kwangsoo <36394194+gitforsteven@users.noreply.github.com>
2025-02-04 00:50:55 +0900

    1. Spring Security jwt

---
+++ src/main/java/kwangsoo/study/springauth/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,57 @@
+package kwangsoo.study.springauth.jwt;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collection;
+import kwangsoo.study.springauth.dto.LoginRequestDto;
+import kwangsoo.study.springauth.entity.UserRoleEnum;
+import kwangsoo.study.springauth.security.UserDetailsImpl;
+import kwangsoo.study.springauth.util.JwtUtil;
+import lombok.Generated;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    @Generated
+    private static final Logger log = LoggerFactory.getLogger("로그인 및 JWT 생성");
+    private final JwtUtil jwtUtil;
+
+    public JwtAuthenticationFilter(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+        this.setFilterProcessesUrl("/api/user/login");
+    }
+
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        log.info("로그인 시도");
+
+        try {
+            LoginRequestDto requestDto = (LoginRequestDto) (new ObjectMapper()).readValue(request.getInputStream(), LoginRequestDto.class);
+            return this.getAuthenticationManager().authenticate(new UsernamePasswordAuthenticationToken(requestDto.getUsername(), requestDto.getPassword(), (Collection) null));
+        } catch (IOException e) {
+            log.error(e.getMessage());
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        log.info("로그인 성공 및 JWT 생성");
+        String username = ((UserDetailsImpl) authResult.getPrincipal()).getUsername();
+        UserRoleEnum role = ((UserDetailsImpl) authResult.getPrincipal()).getUser().getRole();
+        String token = this.jwtUtil.createToken(username, role);
+        this.jwtUtil.addJwtToCookie(token, response);
+    }
+
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+        log.info("로그인 실패");
+        response.setStatus(401);
+    }
+}
---
+++ src/main/java/kwangsoo/study/springauth/jwt/JwtAuthorizationFilter.java
@@ -0,0 +1,67 @@
+package kwangsoo.study.springauth.jwt;
+
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import kwangsoo.study.springauth.security.UserDetailsServiceImpl;
+import kwangsoo.study.springauth.util.JwtUtil;
+import lombok.Generated;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+    @Generated
+    private static final Logger log = LoggerFactory.getLogger("JWT 검증 및 인가");
+    private final JwtUtil jwtUtil;
+    private final UserDetailsServiceImpl userDetailsService;
+
+    public JwtAuthorizationFilter(JwtUtil jwtUtil, UserDetailsServiceImpl userDetailsService) {
+        this.jwtUtil = jwtUtil;
+        this.userDetailsService = userDetailsService;
+    }
+
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain filterChain) throws ServletException, IOException {
+        String tokenValue = this.jwtUtil.getTokenFromRequest(req);
+        if (StringUtils.hasText(tokenValue)) {
+            tokenValue = this.jwtUtil.substringToken(tokenValue);
+            log.info(tokenValue);
+            if (!this.jwtUtil.validateToken(tokenValue)) {
+                log.error("Token Error");
+                return;
+            }
+
+            Claims info = this.jwtUtil.getUserInfoFromToken(tokenValue);
+
+            try {
+                this.setAuthentication(info.getSubject());
+            } catch (Exception e) {
+                log.error(e.getMessage());
+                return;
+            }
+        }
+
+        filterChain.doFilter(req, res);
+    }
+
+    public void setAuthentication(String username) {
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        Authentication authentication = this.createAuthentication(username);
+        context.setAuthentication(authentication);
+        SecurityContextHolder.setContext(context);
+    }
+
+    private Authentication createAuthentication(String username) {
+        UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+        return new UsernamePasswordAuthenticationToken(userDetails, (Object)null, userDetails.getAuthorities());
+    }
+}
