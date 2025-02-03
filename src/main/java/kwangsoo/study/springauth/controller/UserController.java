493a6b843f3ee553f99e96ad05dd750ef98f8f70
Park Kwangsoo <36394194+gitforsteven@users.noreply.github.com>
2025-02-04 01:23:12 +0900

    1. Validation Exception

--- src/main/java/kwangsoo/study/springauth/controller/UserController.java
+++ src/main/java/kwangsoo/study/springauth/controller/UserController.java
@@ -1,37 +1,29 @@
 package kwangsoo.study.springauth.controller;
 
-import jakarta.servlet.http.HttpServletResponse;
-import kwangsoo.study.springauth.auth.AuthController;
 import kwangsoo.study.springauth.dto.LoginRequestDto;
 import kwangsoo.study.springauth.dto.SignupRequestDto;
 import kwangsoo.study.springauth.service.UserService;
-import lombok.RequiredArgsConstructor;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
+@Slf4j
 @Controller
 @RequestMapping("/api")
-@RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
-    @PostMapping("/user/signup")
-    public String signup(SignupRequestDto request) {
-        userService.signup(request);
-        return "redirect:/api/user/login-page";
-    }
-
-    @PostMapping("/user/login")
-    public String login(LoginRequestDto request, HttpServletResponse res) {
-        try {
-            userService.login(request, res);
-        } catch (Exception e) {
-            return "redirect:/api/user/login-page?error";
-        }
-        return "redirect:/";
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @GetMapping("/user/login-page")
@@ -42,5 +34,21 @@ public class UserController {
     @GetMapping("/user/signup")
     public String signupPage() {
         return "signup";
+    }
+
+    @PostMapping("/user/signup")
+    public String signup(@Valid SignupRequestDto requestDto, BindingResult bindingResult) {
+        // Validation 예외처리
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        if(fieldErrors.size() > 0) {
+            for (FieldError fieldError : bindingResult.getFieldErrors()) {
+                log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
+            }
+            return "redirect:/api/user/signup";
+        }
+
+        userService.signup(requestDto);
+
+        return "redirect:/api/user/login-page";
     }
 }
